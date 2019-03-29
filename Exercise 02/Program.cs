6f426f0e90ca653d6cd45750f41547eacf94827b
nineOneOnePawel <p_gajewski@hotmail.com>
2019-03-29 12:23:10 +0000

    commit

---
+++ Exercise 02/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String[] str = new String[20];
+            str[0] = "EnglishMarket01#";
+            str[1] = "SaintFinBarRes2#";
+            str[2] = "BrockCastle03$";
+            str[3] = "ShandonBeLls04$";
+            str[4] = "CorkGhostTour05?";
+            str[5] = "TriskelArts06?";
+            str[6] = "ModelRailway%07";
+            str[7] = "CrosShaven%08";
+            str[8] = "GarnishIsland-09";
+            str[9] = "LoughHyne-10";
+            str[10] = "TheLough1+1";
+            str[11] = "BaLlymaloe+12";
+            str[12] = "Inchydoney@13";
+            str[13] = "LiscarRolL@14";
+            str[14] = "Baltimore^15";
+            str[15] = "CobhQueEnstown^6";
+            str[16] = "FotaWildlife&17";
+            str[17] = "CapeClear&18";
+            str[18] = "CamdenFort:19";
+            str[19] = "GouganeBaRra:20";
+            for (int i = 0; i < str.Length; i++)
+            {
+                Console.WriteLine("Password: " + str[i] + " is valid: " + Password.isPassword(str[i]) + "\n");
+            }
+
+            Console.WriteLine("\n--------------------------------------------------");
+            Console.WriteLine("\nInvalid Passwords");
+            Console.WriteLine("\n--------------------------------------------------\n");
+            String[] notPasswords = new String[9];
+            notPasswords[0] = ""; // min of 8 characters
+            notPasswords[1] = "Abcdefg"; // min of 8 characters            
+            notPasswords[2] = "Abcdefghijklmnopr"; // max of 16 characters
+            notPasswords[3] = "abcdefgh"; // at least one upper case
+            notPasswords[4] = "ABCDEFGH"; // at least one lower case
+            notPasswords[5] = "Abcdefghijklmnop"; // at least one number
+            notPasswords[6] = "Abcdefghijklmno2"; // at least one symbol or special character
+            notPasswords[7] = "AAcdefgh jklmn#2"; // no whitespace
+            notPasswords[8] = "AAcdefghijklmn#2"; // no consecutive identical characters
+            for (int i = 0; i < notPasswords.Length; i++)
+            {
+                bool validPassword = Password.isPassword(notPasswords[i]);
+                Console.WriteLine("Password: " + notPasswords[i] + " is valid: " + validPassword + "\n");
+                if (!validPassword)
+                {
+                    Console.WriteLine(Password.getLastErrorMessage() + "\n");
+                }
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercise 02/SimpleGenericLoginSystem/SimpleGenericLoginSystem/LoginForm.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace SimpleGenericLoginSystem
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelUserID = new System.Windows.Forms.Label();
+            this.labelUserPassword = new System.Windows.Forms.Label();
+            this.textBoxUserID = new System.Windows.Forms.TextBox();
+            this.textBoxUserPassword = new System.Windows.Forms.TextBox();
+            this.buttonUserLogin = new System.Windows.Forms.Button();
+            this.linkLabelForgotPassword = new System.Windows.Forms.LinkLabel();
+            this.labelNewUser = new System.Windows.Forms.Label();
+            this.buttonUserRegister = new System.Windows.Forms.Button();
+            this.linkLabelAdminLogin = new System.Windows.Forms.LinkLabel();
+            this.linkLabelHelp = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // labelUserID
+            // 
+            this.labelUserID.AutoSize = true;
+            this.labelUserID.Location = new System.Drawing.Point(86, 120);
+            this.labelUserID.Name = "labelUserID";
+            this.labelUserID.Size = new System.Drawing.Size(46, 13);
+            this.labelUserID.TabIndex = 0;
+            this.labelUserID.Text = "User ID:";
+            this.labelUserID.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelUserPassword
+            // 
+            this.labelUserPassword.AutoSize = true;
+            this.labelUserPassword.Location = new System.Drawing.Point(87, 151);
+            this.labelUserPassword.Name = "labelUserPassword";
+            this.labelUserPassword.Size = new System.Drawing.Size(56, 13);
+            this.labelUserPassword.TabIndex = 1;
+            this.labelUserPassword.Text = "Password:";
+            // 
+            // textBoxUserID
+            // 
+            this.textBoxUserID.Location = new System.Drawing.Point(145, 117);
+            this.textBoxUserID.MaxLength = 8;
+            this.textBoxUserID.Name = "textBoxUserID";
+            this.textBoxUserID.Size = new System.Drawing.Size(132, 20);
+            this.textBoxUserID.TabIndex = 2;
+            // 
+            // textBoxUserPassword
+            // 
+            this.textBoxUserPassword.Location = new System.Drawing.Point(145, 148);
+            this.textBoxUserPassword.MaxLength = 16;
+            this.textBoxUserPassword.Name = "textBoxUserPassword";
+            this.textBoxUserPassword.Size = new System.Drawing.Size(132, 20);
+            this.textBoxUserPassword.TabIndex = 3;
+            // 
+            // buttonUserLogin
+            // 
+            this.buttonUserLogin.Location = new System.Drawing.Point(297, 326);
+            this.buttonUserLogin.Name = "buttonUserLogin";
+            this.buttonUserLogin.Size = new System.Drawing.Size(75, 23);
+            this.buttonUserLogin.TabIndex = 4;
+            this.buttonUserLogin.Text = "Login";
+            this.buttonUserLogin.UseVisualStyleBackColor = true;
+            // 
+            // linkLabelForgotPassword
+            // 
+            this.linkLabelForgotPassword.AutoSize = true;
+            this.linkLabelForgotPassword.Location = new System.Drawing.Point(281, 310);
+            this.linkLabelForgotPassword.Name = "linkLabelForgotPassword";
+            this.linkLabelForgotPassword.Size = new System.Drawing.Size(91, 13);
+            this.linkLabelForgotPassword.TabIndex = 5;
+            this.linkLabelForgotPassword.TabStop = true;
+            this.linkLabelForgotPassword.Text = "Forgot password?";
+            // 
+            // labelNewUser
+            // 
+            this.labelNewUser.AutoSize = true;
+            this.labelNewUser.Location = new System.Drawing.Point(12, 310);
+            this.labelNewUser.Name = "labelNewUser";
+            this.labelNewUser.Size = new System.Drawing.Size(79, 13);
+            this.labelNewUser.TabIndex = 6;
+            this.labelNewUser.Text = "Not a member?";
+            // 
+            // buttonUserRegister
+            // 
+            this.buttonUserRegister.Location = new System.Drawing.Point(12, 326);
+            this.buttonUserRegister.Name = "buttonUserRegister";
+            this.buttonUserRegister.Size = new System.Drawing.Size(75, 23);
+            this.buttonUserRegister.TabIndex = 7;
+            this.buttonUserRegister.Text = "Register";
+            this.buttonUserRegister.UseVisualStyleBackColor = true;
+            // 
+            // linkLabelAdminLogin
+            // 
+            this.linkLabelAdminLogin.AutoSize = true;
+            this.linkLabelAdminLogin.Location = new System.Drawing.Point(142, 339);
+            this.linkLabelAdminLogin.Name = "linkLabelAdminLogin";
+            this.linkLabelAdminLogin.Size = new System.Drawing.Size(96, 13);
+            this.linkLabelAdminLogin.TabIndex = 8;
+            this.linkLabelAdminLogin.TabStop = true;
+            this.linkLabelAdminLogin.Text = "Administrator Login";
+            // 
+            // linkLabelHelp
+            // 
+            this.linkLabelHelp.AutoSize = true;
+            this.linkLabelHelp.Location = new System.Drawing.Point(12, 9);
+            this.linkLabelHelp.Name = "linkLabelHelp";
+            this.linkLabelHelp.Size = new System.Drawing.Size(35, 13);
+            this.linkLabelHelp.TabIndex = 9;
+            this.linkLabelHelp.TabStop = true;
+            this.linkLabelHelp.Text = "Help?";
+            // 
+            // Login
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.linkLabelHelp);
+            this.Controls.Add(this.linkLabelAdminLogin);
+            this.Controls.Add(this.buttonUserRegister);
+            this.Controls.Add(this.labelNewUser);
+            this.Controls.Add(this.linkLabelForgotPassword);
+            this.Controls.Add(this.buttonUserLogin);
+            this.Controls.Add(this.textBoxUserPassword);
+            this.Controls.Add(this.textBoxUserID);
+            this.Controls.Add(this.labelUserPassword);
+            this.Controls.Add(this.labelUserID);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Login";
+            this.Text = "Login Window";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelUserID;
+        private System.Windows.Forms.Label labelUserPassword;
+        private System.Windows.Forms.TextBox textBoxUserID;
+        private System.Windows.Forms.TextBox textBoxUserPassword;
+        private System.Windows.Forms.Button buttonUserLogin;
+        private System.Windows.Forms.LinkLabel linkLabelForgotPassword;
+        private System.Windows.Forms.Label labelNewUser;
+        private System.Windows.Forms.Button buttonUserRegister;
+        private System.Windows.Forms.LinkLabel linkLabelAdminLogin;
+        private System.Windows.Forms.LinkLabel linkLabelHelp;
+    }
+}
+
